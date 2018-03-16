9774eb1ef6e24af34e01e967f348c34ce870cce3
iamspsarathy <35688629+iamspsarathy@users.noreply.github.com>
2018-03-16 03:47:46 -0700

    Create 073.C

---
+++ 073.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+{
+int a,b,c;
+printf("enter the number:");
+scanf("%d",&a);
+printf("left and right numbers:");
+scanf("%d%d",&b,&c);
+if(a>b&&a<c)
+{
+	printf("yes");
+}
+else
+{
+	printf("no");
+}
+getch()
+}
+return 0;
+}
